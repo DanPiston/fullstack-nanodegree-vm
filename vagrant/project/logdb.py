9a9064c1c6f35e2a4a3e0074e7b816f51a8ca2ff
Dan Piston <danpiston@gmail.com>
2017-05-15 22:40:32 -0400

    Adding project folder

--- vagrant/project/logdb.py
+++ vagrant/project/logdb.py
@@ -5,6 +5,7 @@ from pprint import pprint
 DBNAME = 'news'
 
 def get_top_three():
+    """Gets the most popular 3 posts from log"""
     db = psycopg2.connect(database = DBNAME)
     c = db.cursor()
     query = """
@@ -19,5 +20,8 @@ def get_top_three():
     db.close()
     for entry in data:
         print("The article {} had {} visits".format(entry[0], entry[1]))
+#TODO Who are the most popular article authors of all time?
+
+#TODO Which days did more than 1% of requests lead to errors?
 
 get_top_three()
