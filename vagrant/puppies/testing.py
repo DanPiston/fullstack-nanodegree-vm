4d439af797b9455fc4e285a2baed4cd6208cc8cc
Dan Piston <danpiston@gmail.com>
2017-05-25 22:28:16 -0400

    Starting quests

---
+++ vagrant/puppies/testing.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker
+from puppies import Base, Shelter, Puppy
+
+engine = create_engine('sqlite:///puppyshelter.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+'''
+Using SQLAlchemy perform the following queries on your database:
+
+
+3. Query all puppies by ascending weight
+
+4. Query all puppies grouped by the shelter in which they are staying
+'''
+#1. Query all of the puppies and return the results in ascending alphabetical order
+all_puppies = session.query(Puppy).order_by(Puppy.name).all()
+for pup in all_puppies:
+    print(pup.name)
+
+#2. Query all of the puppies that are less than 6 months old organized by the youngest first
+
+#spinach = session.query(MenuItem).filter_by(name = 'Spinach Ice Cream').one()
+#print(spinach.restaurant.name)
+#session.delete(spinach)
+##session.commit()
+#spinach = session.query(MenuItem).filter_by(name = 'Spinach Ice Cream').one()
